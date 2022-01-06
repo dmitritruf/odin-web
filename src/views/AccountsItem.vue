208773d3f99ccdcfb57abb820be784f4a1e2ff3b
dmitritruf <gttim2@gmail.com>
2022-01-06 13:38:08 +0200

    refactoring accountItem page

--- src/views/AccountsItem.vue
+++ src/views/AccountsItem.vue
@@ -1,144 +1,129 @@
 <template>
-  <div class="container">
-    <div class="block-item">
-      <div class="block-item__title">
-        <BackButton />
-        <h1 class="block-name">Account</h1>
-        <CopyText
-          :text="route.params.hash"
-          :title="route.params.hash"
-          :displayText="route.params.hash"
-          :class-name="'block-id'"
-        />
-      </div>
-      <div class="stats-wrapper mg-b32">
-        <div class="stats-info">
-          <div class="stats-row">
-            <span class="title">GEO Balance:</span>
-            <span class="amount">{{ geoBalance }} GEO</span>
-          </div>
-          <div class="stats-row">
-            <span class="title">ODIN Balance:</span>
-            <span class="amount">{{ odinBalance }} ODIN</span>
-          </div>
+  <div class="view-main">
+    <div class="view-main__title-wrapper">
+      <BackButton text="Top accounts" />
+      <h2 class="view-main__title">Account</h2>
+      <CopyText
+        :text="route.params.hash"
+        :title="route.params.hash"
+        :displayText="route.params.hash"
+        :class-name="'block-id'"
+      />
+    </div>
+
+    <div class="view-main__stats-wrapper mg-b32">
+      <div class="view-main__stats">
+        <div class="view-main__stats-row">
+          <span class="view-main__stats-title">GEO Balance:</span>
+          <span class="view-main__stats-amount">{{ geoBalance }} GEO</span>
+        </div>
+        <div class="view-main__stats-row">
+          <span class="view-main__stats-title">ODIN Balance:</span>
+          <span class="view-main__stats-amount">{{ odinBalance }} ODIN</span>
         </div>
       </div>
-      <h1 class="view-main__subtitle mg-b32">
-        <span>Transactions</span>
-        <small v-if="totalTxCount">{{ totalTxCount }}</small>
-      </h1>
-      <div class="app-table">
-        <div class="data-sources__table-head app-table__head validators-head">
+    </div>
+
+    <h1 class="view-main__subtitle mg-b32">
+      <span>Transactions</span>
+      <small v-if="totalTxCount">{{ totalTxCount }}</small>
+    </h1>
+
+    <div class="app-table">
+      <div class="app-table__head">
+        <span> Transaction hash </span>
+        <span> Type </span>
+        <span> Block </span>
+        <span> Date and time </span>
+        <span> Sender </span>
+        <span> Receiver </span>
+        <span> Amount </span>
+        <span> Transaction fee </span>
+      </div>
+      <template v-if="transactions?.length">
+        <div
+          v-for="(item, index) in transactions"
+          :key="index"
+          class="app-table__row"
+        >
           <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Transaction hash </span>
+            <span class="app-table__title">Transaction hash</span>
+            <TitledLink
+              v-if="item.hash && item.block"
+              :to="`/transactions/${item.block}/${item.hash}`"
+              class="app-table__cell-txt"
+              :text="`0x${item.hash}`"
+            />
+            <span class="app-table__cell-txt" v-else> - </span>
           </div>
           <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Type </span>
+            <span class="app-table__title">Type</span>
+            <span v-if="item.type" :title="item.type">
+              {{ item.type }}
+            </span>
+            <span class="app-table__cell-txt" v-else> - </span>
           </div>
           <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Block </span>
-          </div>
-          <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Date and time </span>
-          </div>
-          <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Sender </span>
-          </div>
-          <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Receiver </span>
-          </div>
-          <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Amount </span>
-          </div>
-          <div class="app-table__cell">
-            <span class="app-table__cell-txt"> Transaction fee </span>
-          </div>
-        </div>
-        <template v-if="transactions?.length">
-          <div
-            v-for="(item, index) in transactions"
-            :key="index"
-            class="data-sources__table-row app-table__row validators-row"
-          >
-            <div class="app-table__cell">
-              <span class="app-table__header">Transaction hash</span>
+            <span class="app-table__title">Block</span>
+            <span v-if="item.block" class="app-table__cell-txt">
               <TitledLink
-                v-if="item.hash && item.block"
-                :to="`/transactions/${item.block}/${item.hash}`"
+                :to="`/blocks/${item.block}`"
                 class="app-table__cell-txt"
-                :text="`0x${item.hash}`"
+                :text="item.block"
               />
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Type</span>
-              <span v-if="item.type" :title="item.type">
-                {{ item.type }}
-              </span>
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Block</span>
-              <span v-if="item.block" class="app-table__cell-txt">
-                <TitledLink
-                  :to="`/blocks/${item.block}`"
-                  class="app-table__cell-txt"
-                  :text="item.block"
-                />
-              </span>
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Date and time</span>
-              <div v-if="item.time">
-                <div class="info-value">
-                  {{ convertToTxTime(item.time) }}
-                </div>
+            </span>
+            <span class="app-table__cell-txt" v-else> - </span>
+          </div>
+          <div class="app-table__cell">
+            <span class="app-table__title">Date and time</span>
+            <div v-if="item.time">
+              <div class="info-value">
+                {{ convertToTxTime(item.time) }}
               </div>
-              <span class="app-table__cell-txt" v-else> - </span>
             </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Sender</span>
-              <TitledLink
-                v-if="item.sender"
-                :to="item.sender"
-                class="app-table__cell-txt"
-                :text="`0x${item.sender}`"
-              />
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Reciever</span>
-              <TitledLink
-                v-if="item.receiver !== ''"
-                :to="item.receiver"
-                class="app-table__cell-txt"
-                :text="`0x${item.receiver}`"
-              />
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Amount</span>
-              <span v-if="item.amount">
-                {{ item.amount }}
-              </span>
-              <span class="app-table__cell-txt" v-else> - </span>
-            </div>
-            <div class="app-table__cell">
-              <span class="app-table__header">Transaction fee</span>
-              <span class="app-table__cell-txt" v-if="item.fee">
-                {{ item.fee }}
-              </span>
-              <span class="app-table__cell-txt" v-else>-</span>
-            </div>
+            <span class="app-table__cell-txt" v-else> - </span>
           </div>
-        </template>
-        <template v-else>
-          <div class="app-table__row">
-            <p class="app-table__empty-stub">No items yet</p>
+          <div class="app-table__cell">
+            <span class="app-table__title">Sender</span>
+            <TitledLink
+              v-if="item.sender"
+              :to="item.sender"
+              class="app-table__cell-txt"
+              :text="`0x${item.sender}`"
+            />
+            <span class="app-table__cell-txt" v-else> - </span>
           </div>
-        </template>
-      </div>
+          <div class="app-table__cell">
+            <span class="app-table__title">Reciever</span>
+            <TitledLink
+              v-if="item.receiver !== ''"
+              :to="item.receiver"
+              class="app-table__cell-txt"
+              :text="`0x${item.receiver}`"
+            />
+            <span class="app-table__cell-txt" v-else> - </span>
+          </div>
+          <div class="app-table__cell">
+            <span class="app-table__title">Amount</span>
+            <span v-if="item.amount">
+              {{ item.amount }}
+            </span>
+            <span class="app-table__cell-txt" v-else> - </span>
+          </div>
+          <div class="app-table__cell">
+            <span class="app-table__title">Transaction fee</span>
+            <span class="app-table__cell-txt" v-if="item.fee">
+              {{ item.fee }}
+            </span>
+            <span class="app-table__cell-txt" v-else>-</span>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <div class="app-table__row">
+          <p class="app-table__empty-stub">No items yet</p>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -187,12 +172,6 @@ export default defineComponent({
           'odin',
           Bech32.decode(route.params.hash as string).data
         )
-        console.debug(
-          'message.sender',
-          await callers.getTxSearch({
-            query: `message.sender='${validatorAddress}'`,
-          })
-        )
 
         const { txs } = await callers.getTxSearch({
           query: `message.sender='${validatorAddress}'`,
@@ -203,8 +182,7 @@ export default defineComponent({
         transactions.value = await prepareTransaction(txs)
         totalTxCount.value = transactions.value.length
       } catch (e) {
-        console.error(e)
-        handleError(e)
+        handleError(e as Error)
       }
     }
 
@@ -226,311 +204,48 @@ export default defineComponent({
   },
 })
 </script>
+
 <style lang="scss" scoped>
-.block {
-  &-item {
-    padding: 2.6rem 3.3rem;
-
-    &__title {
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-      font-weight: 400;
-      margin-bottom: 3.2rem;
-
-      & > * {
-        margin-right: 20px;
-      }
-
-      @media screen and (max-width: 600px) {
-        align-items: center;
-      }
-    }
-
-    @media screen and (max-width: 600px) {
-      padding: 1.6rem;
-    }
+.view-main {
+  &__title {
+    margin: 0 1.6rem 0 2rem;
   }
 
-  &-name {
-    font-weight: 400;
-    font-size: 32px;
-
-    @media screen and (max-width: 600px) {
-      font-size: 28px;
-    }
-  }
-
-  &-row {
-    display: flex;
-    padding-bottom: 1.6rem;
-    padding-top: 1.6rem;
-    border-bottom: 1px solid #cce4ff;
-
-    &__title {
-      max-width: 209px;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: flex-start;
-
-      img {
-        display: block;
-        margin-right: 9px;
-      }
-    }
-  }
-
-  &-id {
-    font-size: 24px;
-    font-weight: 400;
-    text-transform: uppercase;
-
-    @media screen and (max-width: 1000px) {
-      text-overflow: ellipsis;
-      max-width: 50vw;
-      overflow: hidden;
-    }
-
-    @media screen and (max-width: 600px) {
-      max-width: 80vw;
-    }
-  }
-
-  &-chars {
-    padding-top: 1.9rem;
-
-    .app-table__row {
-      display: grid;
-      grid-template-columns: 207px 1fr;
-      padding: 1.6rem 0 1.6rem;
-
-      @media screen and (max-width: 600px) {
-        grid-template-columns: 150px 1fr;
-      }
-    }
-  }
-
-  &-back {
-    min-width: 16px;
-
-    span {
-      display: none;
-    }
-
-    @media screen and (max-width: 600px) {
-      display: flex;
-      align-items: center;
-
-      span {
-        display: inline-block;
-        font-size: 16px;
-        color: var(--clr__action);
-        margin-left: 10px;
-      }
-    }
-  }
-}
-
-.info {
-  &-value {
-    display: flex;
-    align-items: center;
-
-    @media screen and (max-width: 992px) {
-      span {
-        max-width: 250px;
-        text-overflow: ellipsis;
-        overflow: hidden;
-      }
-    }
-
-    @media screen and (max-width: 500px) {
-      span {
-        max-width: 150px;
-        text-overflow: ellipsis;
-        overflow: hidden;
-      }
-    }
-  }
-
-  &-key {
-    display: flex;
-    width: 207px;
-    align-items: center;
-
-    img {
-      display: block;
-      margin-right: 9px;
-      cursor: pointer;
-    }
-
-    div {
-      position: relative;
-      display: flex;
-    }
-
-    @media screen and (max-width: 600px) {
-      width: 150px;
-
-      img {
-        display: none;
-      }
-    }
-  }
-}
-
-.text-blue {
-  color: var(--clr__action);
-}
-
-.copy-button {
-  display: block;
-  margin-left: 10px;
-  width: 13px;
-  height: 15px;
-
-  &__wrapper {
-    position: relative;
-
-    @media screen and (max-width: 1800px) {
-      .tooltip {
-        left: auto;
-        right: -10px;
-        bottom: 25px;
-
-        &:before {
-          right: 9px;
-          left: auto;
-          top: 50px;
-        }
-      }
-    }
-  }
-}
-
-.info-key__inner img:hover + .tooltip,
-.copy-button:hover + .tooltip {
-  opacity: 1;
-}
-
-.tooltip {
-  opacity: 0;
-  position: absolute;
-  left: -29px;
-  bottom: 25px;
-  transition: all 0.15s ease;
-  border-radius: 10px;
-  white-space: nowrap;
-  background: var(--clr__tooltip-new);
-  padding: 12px 24px;
-  color: #fff;
-  pointer-events: none;
-  z-index: 1;
-
-  &:before {
-    content: '';
-    border-top: 10px solid var(--clr__tooltip-new);
-    border-right: 10px solid transparent;
-    border-left: 10px solid transparent;
-    position: absolute;
-    left: 35px;
-    top: 51px;
-    transform: translateY(-50%);
-  }
-
-  @media screen and (max-width: 1300px) {
-    left: auto;
-    right: 0;
-    top: -300%;
-
-    &:before {
-      left: 90%;
-      top: 108%;
-      border-top: 10px solid var(--clr__tooltip-new);
-      border-right: 10px solid transparent;
-      border-left: 10px solid transparent;
-      border-bottom: none;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    display: none;
-  }
-}
-
-.info-key__inner .tooltip {
-  &:before {
-    left: 27px;
-  }
-}
-
-.hash-wrapper {
-  display: flex;
-  align-items: center;
-}
-
-.data-sources__table-head {
-  @media screen and (max-width: 992px) {
-    display: none;
-  }
-}
-
-.app-table__header {
-  display: none;
-
-  @media screen and (max-width: 76.8rem) {
-    display: inline-block;
-    width: 20rem;
-  }
-}
-
-.stats {
-  &-wrapper {
-    border: 1px solid var(--clr__action);
-    border-radius: 8px;
+  &__stats-wrapper {
+    border: 0.1rem solid var(--clr__action);
+    border-radius: 0.8rem;
     padding: 3.2rem 2.4rem;
     display: inline-block;
+  }
 
-    .title {
-      font-size: 16px;
-      width: 100px;
-      display: inline-block;
-      // margin-bottom: 24px;
-    }
+  &__stats-row {
+    display: grid;
+    grid-template-columns: 10rem 1fr;
+    align-items: center;
 
-    .amount {
-      font-weight: 600;
-      font-size: 16px;
+    &:not(:last-child) {
+      margin-bottom: 2.4rem;
     }
   }
 
-  &-row {
-    display: grid;
-    grid-template-columns: 100px 1fr;
-    // margin-bottom: 24px;
-    align-items: center;
-    font-size: 16px;
+  &__stats-title {
+    font-size: 1.6rem;
+    width: 10rem;
+    display: inline-block;
+  }
 
-    span + span {
-      margin-left: 10px;
-      display: inline-block;
-    }
-
-    .amount {
-      font-weight: 600;
-    }
-
-    & + .stats-row {
-      margin-top: 24px;
-    }
+  &__stats-amount {
+    font-weight: 600;
+    font-size: 1.6rem;
+    margin-left: 2.4rem;
   }
 }
 
-.app-table__cell-txt {
-  display: inline-block;
-  max-width: 100px;
-  text-overflow: ellipsis;
-  overflow: hidden;
+@include respond-to(768px) {
+  .view-main {
+    &__title {
+      margin: 0.8rem 0 0.4rem 0;
+    }
+  }
 }
 </style>
